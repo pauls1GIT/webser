8e70d6373bce12544c344f01ffea4bc91af9fd4e
pauls1GIT <psavu22@yahoo.ro>
2021-12-04 04:19:48 +0200

    added test

--- webserver/src/org/junit/Test.java
+++ webserver/src/org/junit/Test.java
@@ -1,6 +1,29 @@
 package org.junit;
+import static org.junit.Assert.*;
 
 public class Test {
+	static Test test;
+	
 
-}
+	    @BeforeClass
+	    public static void beforeClass() {
+	        System.out.println("@BeforeClass");
+	        test = new Test();
+	    }
+
+	    @Test
+	    public void testInt() {
+	        System.out.println("Test");
+	        assertEquals(0, test);
+	    }
+
+	    @Test
+	    public void testString() {
+	        System.out.println("Test String");
+	        assertEquals(1, test);
+	    }
+
+	  
+	}
+
 
